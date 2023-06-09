8a62bd2c619445c54a5fa2a646c2ebf3e99f7b00
Sanyam Jain <sanyamj138@gmail.com>
2023-06-09 16:30:44 +0530

    Complete Project

--- app/src/main/java/com/sanyamj138/myapplication/LoginActivity.kt
+++ app/src/main/java/com/sanyamj138/myapplication/LoginActivity.kt
@@ -1,11 +1,32 @@
 package com.sanyamj138.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var forget: TextView
+    lateinit var register: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        forget = findViewById(R.id.txtforgot)
+        register = findViewById(R.id.txtregister)
+
+        forget.setOnClickListener {
+            val intent = Intent(this, ForgetPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
